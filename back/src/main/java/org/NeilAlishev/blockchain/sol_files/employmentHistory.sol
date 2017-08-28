eb609e6c99b683709cc25d6547cfa2bab1dd5567
Alexander Pliskin <alexnetmore@gmail.com>
2017-08-28 15:33:40 +0300

    final fixes

--- back/src/main/java/org/NeilAlishev/blockchain/sol_files/employmentHistory.sol
+++ back/src/main/java/org/NeilAlishev/blockchain/sol_files/employmentHistory.sol
@@ -2,83 +2,115 @@ pragma solidity ^0.4.15;
 
 contract employmentHistory {
 
-    // person ids -> person's employment history
-    mapping (address => EmpRecord[]) public empRecordOf;
-    // organization ids -> list of employees
+    struct Employee {
+    string name;
+    string email;
+    string position;
+    string city;
+    }
+
+    struct Org {
+    string name;
+    string sphere;
+    }
+
+    struct Offer {
+    address organization;
+    bool approved;
+    }
+
+    struct EmpRecord {
+    address organization;
+    uint dateCreated;
+    EmploymentStatus status;
+    }
+
+    mapping (address => Employee) public employees;
+    mapping (address => Org) public orgs;
+
+    // employee address -> employee`s offers
+    mapping (address => Offer[]) public offersOf;
+
+    // employee address -> employee's employment history
+    mapping (address => EmpRecord[]) public empHistoryOf;
+
+    // organization address -> list of employees
     mapping (address => address[]) public employeesOf;
 
     enum EmploymentStatus { In, Out, Fired }
 
-    struct EmpRecord {
-        address organization;
-        uint dateCreated;
-        EmploymentStatus status;
+    function newEmployee(string name, string email,
+    string position, string city) {
+        require(sha3(email) != sha3(employees[msg.sender].email));
+        employees[msg.sender] = Employee({
+        name: name,
+        email : email,
+        position: position,
+        city: city
+        });
+    }
+
+    function newOrg(string name, string sphere) {
+        require(sha3(name) != sha3(orgs[msg.sender].name));
+        orgs[msg.sender] = Org({
+        name: name,
+        sphere: sphere
+        });
     }
 
     function offer(address employee) {
-        
+        address[] memory empls = employeesOf[msg.sender];
+        for (uint i = 0; i < empls.length; i++) {
+            require(empls[i] != employee);
+        }
+        offersOf[employee].push(Offer({
+        organization: msg.sender,
+        approved: false
+        }));
     }
 
-    function addEmpRecord(uint personId, uint organizationId, uint status) {
-        require(status <= uint(EmploymentStatus.Fired));
-        if (empRecordOf[personId].length == 0) {
-            require(status == uint(EmploymentStatus.In));
-        } else {
-            EmpRecord memory lastRecord = empRecordOf[personId][empRecordOf[personId].length - 1];
-            if (status == uint(EmploymentStatus.In)) {
-                require(uint(lastRecord.status) > uint(EmploymentStatus.In));
-            } else {
-                require(uint(lastRecord.status) == uint(EmploymentStatus.In) &&
-                        lastRecord.organizationId == organizationId);
-            }
-        }
-
-        empRecordOf[personId].push(EmpRecord({
-            organizationId: organizationId,
+    function considerOffer(uint offerIdx, bool approve) {
+        require(offerIdx < offersOf[msg.sender].length);
+        if (approve) {
+            address org = offersOf[msg.sender][offerIdx].organization;
+            empHistoryOf[msg.sender].push(EmpRecord({
+            organization: org,
             dateCreated: now,
-            status: EmploymentStatus(status)
-        }));
-        uint[] storage employeesOfOrg = employeesOf[organizationId];
-        if (status == uint(EmploymentStatus.In)) {
-            employeesOfOrg.push(personId);
+            status: EmploymentStatus.In
+            }));
+            employeesOf[org].push(msg.sender);
         } else {
-            for (uint i = 0; i < employeesOfOrg.length; i++) {
-                if (employeesOfOrg[i] == personId) {
-                    delete employeesOfOrg[i];
-                }
-            }
+            delete offersOf[msg.sender][offerIdx];
         }
     }
 
     // returns -1 if person is unemployed
-    function getCurrentEmployment(uint personId) constant returns (int) {
-        if (empRecordOf[personId].length == 0) {
-            return -1;
+    function getCurrentEmployer() constant returns (address) {
+        if (empHistoryOf[msg.sender].length == 0) {
+            return address(0);
         }
-        EmpRecord memory lastRecord = empRecordOf[personId][empRecordOf[personId].length - 1];
+        EmpRecord memory lastRecord = empHistoryOf[msg.sender]
+        [empHistoryOf[msg.sender].length - 1];
 
         if (lastRecord.status != EmploymentStatus.In) {
-            return -1;
+            return address(0);
         } else {
-            return int(lastRecord.organizationId);
+            return lastRecord.organization;
         }
     }
 
-    function getEmploymentHistory(uint personId) constant returns (uint[]) {
-        EmpRecord[] memory records = empRecordOf[personId];
+    function getEmploymentHistory() constant returns (uint[]) {
+        EmpRecord[] memory records = empHistoryOf[msg.sender];
         uint[] memory result = new uint[](records.length * 3);
 
         for (uint i = 0; i < records.length; i++) {
             uint index = i * 3;
-            result[index] = records[i].organizationId;
+            //TODO convert address to smth reternable
+            result[index] = uint(records[i].organization);
             result[index + 1] = records[i].dateCreated;
             result[index + 2] = uint(records[i].status);
         }
 
         return result;
-    }
-
-    function getOrganisationEmployees(uint organizationId) constant returns (uint[]) {
-        return employeesOf[organizationId];
     }
 }
