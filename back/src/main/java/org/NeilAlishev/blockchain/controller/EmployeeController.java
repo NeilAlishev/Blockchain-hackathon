7a2c140f1278e0e66f12f88af40a7b06009fb968
Neil Alishev <alishev.neil@gmail.com>
2017-08-28 05:10:57 +0300

    add employee history page

--- back/src/main/java/org/NeilAlishev/blockchain/controller/EmployeeController.java
+++ back/src/main/java/org/NeilAlishev/blockchain/controller/EmployeeController.java
@@ -13,12 +13,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class EmployeeController {
 
+    private final EmployeeService employeeService;
+
     @Autowired
-    EmployeeService employmentService;
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
 
     @GetMapping(ApplicationUrls.EMPLOYEE_HISTORY_BASE_URL)
     public String getEmployeeHistory(Model model) {
-        model.addAttribute("records", employmentService.getRecords());
-        return "";
+//        model.addAttribute("records", employeeService.getRecords());
+        return "employment_history";
     }
 }
